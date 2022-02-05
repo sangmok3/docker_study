eb3532dec103acb952c51a58ddc2c59eb864fb8a
sangmok3 <67615844+sangmok3@users.noreply.github.com>
2022-02-05 16:58:34 +0900

    Update create_table.sql

--- database/scripts/create_table.sql
+++ database/scripts/create_table.sql
@@ -1,4 +1,4 @@
 CREATE TABLE visits (
 visitor_name varchar(25) not null,
-visit_datetime datetime default NOW() not null
+visit_datetime datetime default DATE_ADD(NOW(),INTERVAL 9 HOUR) not null
 );
