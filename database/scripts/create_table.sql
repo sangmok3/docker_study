f26e829a93c4f93e1ccd934b6b9a3e105ae18ea6
sangmok3 <sm.lee@coinone.com>
2022-02-06 16:42:00 +0900

    change_comment

--- database/scripts/create_table.sql
+++ database/scripts/create_table.sql
@@ -4,5 +4,5 @@ SET time_zone = 'Asia/Seoul';
 CREATE TABLE visits (
 visitor_name varchar(25) not null,
 visit_datetime datetime default NOW() not null,
-visit_comment varchar(100) default 'no comment' not null,
+visit_comment varchar(100) default null
 );
